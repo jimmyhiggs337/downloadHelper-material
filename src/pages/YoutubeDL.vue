aba024c3123ea0c2e6e3e9f5a6f8dc67af97748e
James Higginbotham IV <45857725+jimmyhiggs337@users.noreply.github.com>
2022-08-01 17:03:45 -0500

    added frontend for some ytdl advanced settings

--- src/pages/YoutubeDL.vue
+++ src/pages/YoutubeDL.vue
@@ -15,18 +15,65 @@
           <q-tab name="File" label="Via txt File" no-caps />
         </q-tabs>
       </q-toolbar>
-      <q-tab-panels v-model="tab" animated>
+      <q-tab-panels v-model="tab">
         <q-tab-panel name="URL">
           <q-card-section>
-            <q-input outlined label="URL" />
+            <q-input
+              outlined
+              v-model="URLs"
+              label="URL(s)"
+              type="textarea"
+              hint="one URL per line, no characters after the URL"
+              :shadow-text="urlShadowText"
+              @keydown="processTextareaFill($event, 'urlShadowText')"
+            />
           </q-card-section>
         </q-tab-panel>
         <q-tab-panel name="File" style="padding: 2rem">
-          <fileUploader label="Select .txt file" types=".txt"></fileUploader>
+          <fileUploader
+            label="Select .txt file"
+            allowedFileTypes=".txt"
+          ></fileUploader>
         </q-tab-panel>
       </q-tab-panels>
       <q-separator />
-      <q-card-actions align="right" class="q-ma-sm">
+      <div class="q-ml-md q-mt-sm text-bold text-grey">Advanced Options</div>
+      <div class="fit row q-px-lg q-pt-sm q-pb-md">
+        <q-select
+          square
+          outlined
+          v-model="downloaders"
+          :options="downloaderOptions"
+          label="Downloader"
+          class="col q-mx-xs"
+        />
+        <q-select
+          square
+          outlined
+          v-model="quality"
+          :options="qualityOptions"
+          label="Quality"
+          class="col q-mx-xs"
+        />
+        <div class="col-12">
+          <div class="row">
+            <q-input
+              square
+              outlined
+              v-model="exportfileType"
+              label="Output File Extension"
+              class="col-shrink q-mx-xs q-my-sm"
+              :shadow-text="convertShadowText"
+              :disable="!convertFile"
+              @keydown="processTextareaFill($event, 'convertShadowText')"
+            />
+            <q-checkbox v-model="convertFile" label="Convert Output file" />
+          </div>
+        </div>
+      </div>
+      <q-separator />
+      <q-card-actions class="q-ma-sm justify-between">
+        <q-checkbox v-model="audioOnly" label="Audio Only" />
         <q-btn class="bg-primary text-white">download</q-btn>
       </q-card-actions>
     </q-card>
@@ -42,9 +89,28 @@ export default {
   },
   setup() {
     return {
+      URLs: ref(""),
       tab: ref("URL"),
-      label: "",
-      types: "",
+      downloaders: ref("default"),
+      audioOnly: ref(false),
+      quality: ref("Max"),
+      exportfileType: ref(""),
+      fileLabel: "",
+      allowedFileTypes: "",
+      convertFile: ref(false),
+      downloaderOptions: [
+        "default",
+        "aria2c",
+        "avconv",
+        "axel",
+        "curl",
+        "ffmpeg",
+        "httpie",
+        "wget",
+      ],
+      qualityOptions: ["Max"],
+      urlShadowText: `ex: \n url1 \n url2`,
+      convertShadowText: `Ex: .mp4`,
     };
   },
   props: {
@@ -55,6 +121,14 @@ export default {
     handleFileChange(e) {
       // Whenever the file changes, emit the 'input' event with the file data.
       this.$emit("input", e.target.files[0]);
+    },
+    processTextareaFill(e, shadowTextName) {
+      if (e === void 0) {
+        return;
+      }
+      if (e.target.value.length > 0) {
+        this[shadowTextName] = "";
+      }
     },
   },
 };
